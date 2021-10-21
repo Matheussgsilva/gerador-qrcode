4f490729df7a2baf28904dabb8bdf75480d53379
Matheussgsilva <matheussgsilva@hotmail.com>
2021-10-21 11:48:14 -0300

    Add conditional for QR code generation

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
-var textInput = document.querySelector("#text-input").value
+var textInput = document.querySelector("#text-input")
 var returnQr = document.querySelector("#return-qr")
 
 function gerar() {
-    returnQr.insertAdjacentHTML('afterbegin', /*html*/`
-    <img src="https://www.qrtag.net/api/qr_transparent_360.png?url=https://www.github.com/matheussgsilva" alt="qr code" id="qr-image">
-    `)
+    if (textInput.value.startsWith("http")) {
+        returnQr.innerHTML = ""
+        returnQr.insertAdjacentHTML('afterbegin', /*html*/`
+        <img src="https://www.qrtag.net/api/qr_transparent_360.png?url=${textInput.value}" alt="qr code" id="qr-image">
+        `)
+    } else {
+        returnQr.insertAdjacentHTML("afterbegin", /*html*/ `
+        <p>Endereço inválido</p>
+        <p>O Endereço deve começar com: https://www ou http://www</p>
+        `)
+    }
 }
