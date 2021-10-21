520329efc86acbf93c06006bb57dc14cf4207dc1
Matheussgsilva <matheussgsilva@hotmail.com>
2021-10-21 12:28:37 -0300

    Create clear function

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,24 @@
 var textInput = document.querySelector("#text-input")
 var returnQr = document.querySelector("#return-qr")
+var generate = document.querySelector("#generate")
+var clear = document.querySelector("#clear")
 
-function gerar() {
+generate.addEventListener("click", function generate() {
     if (textInput.value.startsWith("http")) {
         returnQr.innerHTML = ""
         returnQr.insertAdjacentHTML('afterbegin', /*html*/`
         <img src="https://www.qrtag.net/api/qr_transparent_360.png?url=${textInput.value}" alt="qr code" id="qr-image">
         `)
     } else {
+        returnQr.innerHTML = ""
         returnQr.insertAdjacentHTML("afterbegin", /*html*/ `
         <p>Endereço inválido</p>
         <p>O Endereço deve começar com: https://www ou http://www</p>
         `)
     }
-}
+})
+
+clear.addEventListener("click", function clear() {
+    textInput.value = ""
+    returnQr.innerHTML = ""
+})
